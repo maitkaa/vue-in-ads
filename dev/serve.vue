0f02c306910a7d4e74341edeeb43abbb38d68308
Mait Kaasik <mait.kaasik@gmail.com>
2022-08-16 23:32:24 +0300

    init commit

---
+++ dev/serve.vue
@@ -0,0 +1,31 @@
+<script>
+import { defineComponent } from 'vue';
+import VueInAds from '@/vue-in-ads.vue';
+
+export default defineComponent({
+  name: 'ServeDev',
+  components: {
+    VueInAds
+  },
+  data: () => ({
+    config: {
+      "container": "InAdsContainer",
+      "mode": "1",
+      "nocss": false,
+      "lang": "et",
+      "appartment": 2,
+      "ihist": "1993",
+      "defaultBaseLayer": "ORTOFOTO",
+      "baseLayers": ["ORTOFOTO"],
+      "mapLayers": ["AADRESS", "KATASTRIYKSUS"],
+      "WMS": []
+    }
+  })
+});
+</script>
+
+<template>
+  <div id="app">
+    <vue-in-ads :config="config" />
+  </div>
+</template>
---
+++ src/vue-in-ads.vue
@@ -0,0 +1,39 @@
+<template>
+  <div id="InAdsContainer"></div>
+</template>
+
+<script setup>
+import { loadScript } from "vue-plugin-load-script";
+import { defineProps, onMounted } from 'vue';
+const props = defineProps({
+  config: {
+    type: Object,
+    default: {
+      "container": "InAdsContainer",
+      "mode": "1",
+      "nocss": false,
+      "lang": "et",
+      "appartment": 2,
+      "ihist": "1993",
+      "defaultBaseLayer": "ALUSKAART",
+      "baseLayers": ["ALUSKAART", "ORTOFOTO", "HYBRIID"],
+      "mapLayers": ["AADRESS", "KATASTRIYKSUS"],
+      "WMS": []
+    }
+  }
+});
+const script = "https://inaadress.maaamet.ee/inaadress/js/inaadress.min.js";
+
+onMounted(() => {
+delete props.config.container;
+props.config.container = 'InAdsContainer';
+  loadScript(script)
+    .then(() => {
+      console.info("Loaded:" + script);
+      return new InAadress(props.config);
+    })
+    .catch(() => {
+      console.error("Failed to load:" + script);
+    });
+});
+</script>
